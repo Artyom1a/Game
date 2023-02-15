dd1d6e4702620513be47deef12fda8cc4ad7e4c1
Artyom Shevchik <artyom.shevchik16@gmail.com>
2023-02-15 23:43:50 +0300

    games15.02

---
+++ Program/Program.cs
@@ -0,0 +1,25 @@
+﻿
+using CoreLibrary.Menu;
+using CoreLibrary.Models;
+using CoreLibrary.Repositories;
+//using CoreLibrary.Services;
+
+namespace Programs;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        User user = new User(1,"Artyom","artyom@gmail.com","12345");
+        Console.WriteLine(user.ToString());
+        UserRepository repository = new UserRepository();
+        //var users = repository.GetAll();
+        //Console.WriteLine(string.Join(",", users));
+        repository.Add(new User(1, "Artyom", "artyom@gmail.com", "12345"));
+        repository.Add(new User(2, "Test", "artyom@gmail.com", "12345"));
+        repository.Add(new User(3, "Test2", "Test@gmail.com", "12345"));
+
+
+    }
+}
+
