d702fc4128c899349a62f126963fc14dd6a879e3
Artyom Shevchik <artyom.shevchik16@gmail.com>
2023-02-16 01:47:59 +0300

    games16.02

---
+++ CoreLibrary/Models/User.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace CoreLibrary.Models
+{
+    public class User : IData
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        
+
+
+        public User(int id, string name, string email, string password)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+            Password = password;
+        }
+
+        public override string ToString()
+        {
+            return $"{Id}  {Name} {Email} {Password}";
+        }
+    }
+}
