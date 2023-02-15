a5f30fbf7c891e473ce096339742220817456c46
Artyom Shevchik <artyom.shevchik16@gmail.com>
2023-02-16 01:47:15 +0300

    games15.02

---
+++ CoreLibrary/Repositories/UserStatisticsRepository.cs
@@ -0,0 +1,15 @@
+﻿using CoreLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace CoreLibrary.Repositories
+{
+    public class UserStatisticsRepository : BaseRepository<UserStatistic>
+    {
+        protected override string Path => "C:\\Code Main\\usersstatistic.txt";
+    }
+}
