d5e01a5e9289bd5372e2ac794d8c2ad05c6e2b43
Artyom Shevchik <artyom.shevchik16@gmail.com>
2023-02-16 01:47:41 +0300

    games16.02

---
+++ CoreLibrary/Services/UserService.cs
@@ -0,0 +1,55 @@
+﻿using CoreLibrary.Models;
+using CoreLibrary.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using CoreLibrary.Repositories;
+
+namespace CoreLibrary.Services
+{
+    public class UserService
+    {
+
+        public bool AddUser(List<User> usersvse, User adduser)
+        {
+            try
+            {
+                User adduser1 = new User(adduser.Id, adduser.Name, adduser.Email, adduser.Password);
+                if (string.IsNullOrEmpty(adduser.Name) || string.IsNullOrEmpty(adduser.Email) || string.IsNullOrEmpty(adduser.Password))
+                {
+                    System.Console.WriteLine("error input");
+                    return false;
+                }
+                for (int i = 0; i < usersvse.Count; i++)
+                {
+
+                    if (usersvse[i].Name == adduser1.Name && usersvse[i].Email == adduser1.Email)
+                    {
+                        Console.WriteLine("Name or Email are registrated yet");
+                        return false;
+                    }
+
+                }
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+        }
+    }
+}
+
+
+//for (int i = 0; i < uservse.Count; i++)
+//{
+//    if (adduser.Email == email)//isnull
+//    {
+//        System.Console.WriteLine("This email not available");
+//        return false;
+//    }
+//}
