fbe7b67ca37e781b245791704854e913933e0f64
Artyom Shevchik <artyom.shevchik16@gmail.com>
2023-02-20 13:31:00 +0300

    Game20.02

--- CoreLibrary/Menu/Menu.cs
+++ CoreLibrary/Menu/Menu.cs
@@ -1,7 +1,69 @@
-﻿namespace CoreLibrary.Menu
+﻿using CoreLibrary.Models;
+using CoreLibrary.Controller;
+
+namespace CoreLibrary.Menu
 {
     public class Menu
     {
 
+        public bool MainMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("Menu:");
+            Console.WriteLine("1 Registration");
+            Console.WriteLine("2 Authorizathion");
+            Console.WriteLine("0 Exit");
+            Console.Write("\r\nSelect an option: ");
+
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    UserController userController = new UserController();
+                    (bool, User) result = userController.AddUserController();
+                    return true;
+                case "2":
+                    UserController userController1 = new UserController();
+                    (bool, User) result1 = userController1.Login();
+                    if (result1.Item1 == false)
+                    {
+                        Console.Clear();
+                        Console.WriteLine("This password incorrect");
+                        Console.WriteLine("0 Exit");
+                        switch (Console.ReadLine())
+                        {
+                            case "0":
+                                return false;
+                        }
+                    }
+                    if (result1.Item1 == true)
+                    {
+                        Console.Clear();
+                        Console.WriteLine("1 Games");
+                        Console.WriteLine("2 Delete account");
+                        Console.WriteLine("0 Exit");
+                        switch (Console.ReadLine())
+                        {
+                            case "0":
+                                return false;
+                            case "1": return true;
+                            case "2":
+                                UserController userController3 = new UserController();
+                                userController3.Delete();
+                                return true;
+                        }
+                        return true;
+                    }
+                    return false;
+
+                case "4":
+
+                case "0":
+                    return false;
+                default:
+                    return true;
+            }
+        }
+
     }
 }
+
