d89c8c8c0ff3d78b47130fa8d18f7655890a67b8
Artyom Shevchik <artyom.shevchik16@gmail.com>
2023-02-21 18:46:16 +0300

    Game21.02

---
+++ GameDesktopApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Text;
+using GameDesktopApp.Core;
+
+namespace GameDesktopsApps;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        FieldWar fieldWar = new FieldWar();
+        fieldWar.Display();
+
+
+    }
+}
