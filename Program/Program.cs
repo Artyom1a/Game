033533e1e95e4a17b18d6e4b1fb09bbc71a360e6
Artyom Shevchik <artyom.shevchik16@gmail.com>
2023-02-25 08:54:32 +0300

    Game25.02

--- Program/Program.cs
+++ Program/Program.cs
@@ -2,7 +2,9 @@
 using CoreLibrary.Menu;
 using CoreLibrary.Models;
 using CoreLibrary.Repositories;
+using CoreLibrary.Menu;
 //using CoreLibrary.Services;
+using GameSeaBattle;
 
 namespace Programs;
 
@@ -10,16 +12,28 @@ public class Program
 {
     private static void Main(string[] args)
     {
-        User user = new User(1,"Artyom","artyom@gmail.com","12345");
-        Console.WriteLine(user.ToString());
-        UserRepository repository = new UserRepository();
+
+        Menu menu = new Menu();
+        bool showMenu = true;
+        while (showMenu)
+        {
+            Console.ReadKey();
+            showMenu = menu.MainMenu();
+        }
+
+
+        //User user = new User(1, "Artyom", "artyom@gmail.com", "12345");
+        //Console.WriteLine(user.ToString());
+        //UserRepository repository = new UserRepository();
         //var users = repository.GetAll();
         //Console.WriteLine(string.Join(",", users));
-        repository.Add(new User(1, "Artyom", "artyom@gmail.com", "12345"));
-        repository.Add(new User(2, "Test", "artyom@gmail.com", "12345"));
-        repository.Add(new User(3, "Test2", "Test@gmail.com", "12345"));
+
+        //repository.Add(new User(1, "Artyom", "artyom@gmail.com", "12345"));
+        //repository.Add(new User(2, "Test", "artyom@gmail.com", "12345"));
+        //repository.Add(new User(3, "Test2", "Test@gmail.com", "12345"));
 
 
     }
+
 }
 
