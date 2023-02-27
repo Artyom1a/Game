901fef1d167110c9baf5b9cbf6e961c4cc0f902d
Artyom Shevchik <artyom.shevchik16@gmail.com>
2023-02-27 18:24:25 +0300

    Game27.02

--- GameDesktopApp/Program.cs
+++ GameDesktopApp/Program.cs
@@ -1,5 +1,11 @@
-﻿using System.Text;
+﻿using System.ComponentModel;
+using System.Text;
 using GameSeaBattle;
+//using GameSeaBattle.Games;
+//using GameSeaBattle.Ships;
+using GameSeaBattle.Boards;
+using GameSeaBattle.Players;
+
 
 
 namespace GameDesktopsApps;
@@ -8,9 +14,19 @@ public class Program
 {
     private static void Main(string[] args)
     {
-        FieldWar1 fieldWar1 = new FieldWar1();
-        fieldWar1.Display();
 
 
+        Console.Title = "Battle Ship";
+        var User = new Users();
+        var Bot = new Bots();
+        int yes = 0;
+
+        while (yes < 1)
+        {
+
+            User.Display(User.CopyFieldWar);
+            yes++;
+
+        }
     }
 }
