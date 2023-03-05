214febb5be4cfac5acc38d20613cd4481b480f11
Artyom Shevchik <artyom.shevchik16@gmail.com>
2023-03-05 13:39:09 +0300

    Game05.03

---
+++ CoreLibrary/Services/UserStatisticsService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CoreLibrary.Models;
+using CoreLibrary.Repositories;
+
+namespace CoreLibrary.Services
+{
+    public class UserStatisticsService
+    {
+
+        private readonly UserStatisticsRepository userStatisticsRepository = new UserStatisticsRepository();
+
+        public List<UserStatistic> GetAllStats()
+        {
+            return userStatisticsRepository.GetAllStats();
+        }
+        public List<UserStatistic> GetStatsUser(int id)
+        {
+            return userStatisticsRepository.GetUserStats(id);
+        }
+        public void AddUserStats(UserStatistic userStats)
+        {
+            userStatisticsRepository.AddUserStats(userStats);
+        }
+    }
+}
+
