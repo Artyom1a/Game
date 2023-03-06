ca626524df43ddcbd3f3fa9402bb622ef94c2e91
Artyom Shevchik <artyom.shevchik16@gmail.com>
2023-03-06 22:39:58 +0300

    Game05.03

--- CoreLibrary/Repositories/UserStatisticsRepository.cs
+++ CoreLibrary/Repositories/UserStatisticsRepository.cs
@@ -1,4 +1,5 @@
 ﻿using CoreLibrary.Models;
+using CoreLibrary.Repositories;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,5 +12,33 @@ namespace CoreLibrary.Repositories
     public class UserStatisticsRepository : BaseRepository<UserStatistic>
     {
         protected override string Path => "C:\\Code Main\\usersstatistic.txt";
+
+        public void AddUserStats(UserStatistic userStats)
+        {
+            if (userStats == null) throw new ArgumentNullException(nameof(userStats));
+            List<UserStatistic> users = GetAll().ToList();
+            users.Add(userStats);
+            OverWritingFile(users);
+        }
+
+        public List<UserStatistic> GetUserStats(int id)
+        {
+            List<UserStatistic> users = GetAll().ToList();
+            List<UserStatistic> stats = new List<UserStatistic>();
+            for (int i = 0; i < users.Count; i++)
+            {
+                if (users[i].Id == id)
+                {
+                    stats.Add(users[i]);
+                }
+            }
+            return stats;
+        }
+
+        public List<UserStatistic> GetAllStats()
+        {
+            List<UserStatistic> users = GetAll().ToList();
+            return users;
+        }
     }
 }
