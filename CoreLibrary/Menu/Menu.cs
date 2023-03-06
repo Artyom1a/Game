e2591d3e63578b8e8818316cb11aa7c50ec58290
Artyom Shevchik <artyom.shevchik16@gmail.com>
2023-03-06 22:40:55 +0300

    Game06.03

--- CoreLibrary/Menu/Menu.cs
+++ CoreLibrary/Menu/Menu.cs
@@ -2,6 +2,7 @@
 using CoreLibrary.Controller;
 using GameSeaBattle;
 using GameSeaBattle.Boards;
+using GameSeaBattle.Players;
 
 namespace CoreLibrary.Menu
 {
@@ -42,17 +43,60 @@ namespace CoreLibrary.Menu
                         Console.Clear();
                         Console.WriteLine("1 Games");
                         Console.WriteLine("2 Delete account");
+                        Console.WriteLine("3 Statistics game");
                         Console.WriteLine("0 Exit");
                         switch (Console.ReadLine())
                         {
                             case "0":
                                 return false;
                             case "1":
-                                FieldWar fieldWar = new FieldWar();
-                                
+                                Console.Clear();
+                                Console.WriteLine("1 BattleShip");
+                                Console.WriteLine("0 Exit");
+                                switch (Console.ReadLine())
+                                {
+                                    case "1":
+                                                                             
+                                        UserStatisticsController statistics1 = new UserStatisticsController();
+                                        statistics1.AddUserStat(result1.Item2,  "BattleShip");
+                                        Console.Title = "Battle Ship";
+                                        var User = new Users();
+                                        var Bot = new Bots();
+                                        Boolean yes = true;
+                                        while (yes)
+                                        {
+                                            while (true)
+                                            {
+                                                User.Display(User.CopyFieldWar);
+                                                User.Strike();
+                                                if (User.Win())
+                                                {
+                                                    break;
+                                                }
+                                                Bot.Strike();
+                                                if (Bot.Win())
+                                                {
+                                                    break;
+                                                }
+                                            }
+                                            Console.SetCursorPosition(30, 1);
+                                            Console.WriteLine("Thanks for playing! Do you want to play again? ");
+                                            if (Console.ReadLine() != "Yes")
+                                            {
+                                                Console.Clear();
+                                                yes = false;
+                                            }
+                                        }
+                                        return true;
+                                }
                                 return true;
                             case "2":
                                 userController1.Delete(result1.Item2);
+                                return true;
+                            case "3":
+
+                                UserStatisticsController statistics = new UserStatisticsController();
+                                statistics.GetUserStat(result1.Item2);
                                 return true;
                         }
                         return true;
